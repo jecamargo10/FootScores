5072a84002283624d1e294b5295f63ea7401159b
jecamargo10 <javier_22ca@hotmail.com>
2017-03-06 20:37:25 -0500

    review string base de datos

--- back-end/database/mongo-config.js
+++ back-end/database/mongo-config.js
@@ -1,4 +1,7 @@
 //USER: admin PASS: admin
+
+/*Javier Camargo: No es conveniente tener las credenciales de la base de datos abiertas al publico, tanto a nivel de repositorio como accesibles por codigo
+Una recomendacion es definir el url conection como variable de entorno*/
 module.exports.url = 'mongodb://admin:admin@ds117830.mlab.com:17830/footscores';
 module.exports.user = 'admin';
 module.exports.password = 'admin';
